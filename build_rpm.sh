920717ef53bf8674c5e479ad9364dbf94c811029
Holaday, Sean <Sean_Holaday@comcast.com>
2017-02-16 13:23:19 -0800

    Testing out to see how environment variables in travis work.

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 release=`git describe --abbrev=0 --tags`
+echo $release
+echo $TRAVIS_BUILD_NUMBER
 
-echo "Building the petasos rpm..."
-docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
-docker exec build bash -c "mv petasos petasos-1.1.0; tar -czvf petasos-1.1.0.tar.gz petasos-1.1.0; mv petasos-1.1.0.tar.gz /root/rpmbuild/SOURCES"
-docker exec build bash -c "pushd petasos-1.1.0; rpmbuild -ba petasos.spec; popd"
+# release="${v%.*}.$((${v##*.}+1))"
+# echo $release
+
+# echo "Building the petasos rpm..."
+# docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
+# docker exec build bash -c "mv petasos petasos-1.1.0; tar -czvf petasos-1.1.0.tar.gz petasos-1.1.0; mv petasos-1.1.0.tar.gz /root/rpmbuild/SOURCES"
+# docker exec build bash -c "pushd petasos-1.1.0; rpmbuild -ba petasos.spec; popd"
 
