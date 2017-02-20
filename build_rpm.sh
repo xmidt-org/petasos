73a166fe127eb52ac31f9c0e3dc900f0c91ed628
Holaday, Sean <Sean_Holaday@comcast.com>
2017-02-20 12:52:02 -0800

    Starting to issue commands to docker now.

--- build_rpm.sh
+++ build_rpm.sh
@@ -19,36 +19,21 @@ for ((i=${#tags[@]}-1; i >=0; i--)); do
     fi
 done
 
-echo "Most recent release tag: $release"
+if [ -z "$release"  ]; then
+    echo "Could not find latest release tag!"
+else
+    echo "Most recent release tag: $release"
+fi
 
 IFS=$OIFS
 
-# # begin old script
-# echo "Hello world."
-# touch versionno.txt
-# 
-# echo $(git tag -l)
-# 
-# git for-each-ref --sort=-authordate refs/tags | \
-#     while read entry; do
-#         echo $entry
-#         tag=`echo $entry | awk '{print $NF}'`
-#         tag=`echo $tag | awk -F/ '{print $NF}'`
-#         if [[ "$tag" != *"-"* ]]; then
-#             echo "$tag" > versionno.txt
-#             break
-#         fi
-#     done
-# 
-# release=`cat versionno.txt`
-# rm versionno.txt
-# 
-# filename=`echo "$release" | awk -F. '{$NF+=1; OFS="."; print $0}'`
-# filename+="-${BUILD_NUMBER}alpha"
-# echo $filename
-# 
-# # echo "Building the petasos rpm..."
-# # docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
-# # docker exec build bash -c "mv petasos petasos-1.1.0; tar -czvf petasos-1.1.0.tar.gz petasos-1.1.0; mv petasos-1.1.0.tar.gz /root/rpmbuild/SOURCES"
-# # docker exec build bash -c "pushd petasos-1.1.0; rpmbuild -ba petasos.spec; popd"
-# 
+new_release=`echo "$release" | awk -F. '{$NF+=1; OFS="."; print $0}'`
+new_release+="-${BUILD_NUMBER}alpha"
+echo "Issuing release $new_release..."
+
+echo "Building the petasos rpm..."
+docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
+docker exec build bash -c "mv petasos petasos-$new_release; tar -czvf petasos-$new_release.tar.gz petasos-$new_release; mv petasos-$new_release.tar.gz /root/rpmbuild/SOURCES"
+docker exec build bash -c "pushd /root/rpmbuild; ls -R; popd"
+docker exec build bash -c "pushd petasos-$new_release; rpmbuild -ba petasos.spec; popd"
+
