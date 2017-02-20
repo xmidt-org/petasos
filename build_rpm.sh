f5786146e6646b4c6bab4382c83dacfa6fd17b22
Holaday, Sean <Sean_Holaday@comcast.com>
2017-02-20 11:24:53 -0800

    Finally iterating backwards through an array.

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
 
-echo "Hello world."
-touch versionno.txt
+OIFS=$IFS
+IFS='
 
-echo $(git tag -l)
+'
 
-git for-each-ref --sort=-authordate refs/tags | \
-    while read entry; do
-        echo $entry
-        tag=`echo $entry | awk '{print $NF}'`
-        tag=`echo $tag | awk -F/ '{print $NF}'`
-        if [[ "$tag" != *"-"* ]]; then
-            echo "$tag" > versionno.txt
-            break
-        fi
-    done
+taglist=`git tag -l`
+tags=($taglist)
+echo ${#tags[@]}
 
-release=`cat versionno.txt`
-rm versionno.txt
+for ((i=${#tags[@]}-1; i >=0; i--)); do
+    echo "Element:"
+    echo ${tags[i]}
+done
 
-filename=`echo "$release" | awk -F. '{$NF+=1; OFS="."; print $0}'`
-filename+="-${BUILD_NUMBER}alpha"
-echo $filename
+IFS=$OIFS
 
-# echo "Building the petasos rpm..."
-# docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
-# docker exec build bash -c "mv petasos petasos-1.1.0; tar -czvf petasos-1.1.0.tar.gz petasos-1.1.0; mv petasos-1.1.0.tar.gz /root/rpmbuild/SOURCES"
-# docker exec build bash -c "pushd petasos-1.1.0; rpmbuild -ba petasos.spec; popd"
-
+# # begin old script
+# echo "Hello world."
+# touch versionno.txt
+# 
+# echo $(git tag -l)
+# 
+# git for-each-ref --sort=-authordate refs/tags | \
+#     while read entry; do
+#         echo $entry
+#         tag=`echo $entry | awk '{print $NF}'`
+#         tag=`echo $tag | awk -F/ '{print $NF}'`
+#         if [[ "$tag" != *"-"* ]]; then
+#             echo "$tag" > versionno.txt
+#             break
+#         fi
+#     done
+# 
+# release=`cat versionno.txt`
+# rm versionno.txt
+# 
+# filename=`echo "$release" | awk -F. '{$NF+=1; OFS="."; print $0}'`
+# filename+="-${BUILD_NUMBER}alpha"
+# echo $filename
+# 
+# # echo "Building the petasos rpm..."
+# # docker exec build bash -c "pushd petasos; git fetch; git checkout travis-testing; popd"
+# # docker exec build bash -c "mv petasos petasos-1.1.0; tar -czvf petasos-1.1.0.tar.gz petasos-1.1.0; mv petasos-1.1.0.tar.gz /root/rpmbuild/SOURCES"
+# # docker exec build bash -c "pushd petasos-1.1.0; rpmbuild -ba petasos.spec; popd"
+# 
