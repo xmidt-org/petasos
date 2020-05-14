1f7c652dbb817c289be69b156e1f3919094f6296
Joel Unzain <joel_unzain@comcast.com>
2020-05-13 17:50:39 -0700

    register for specific signals

--- main.go
+++ main.go
@@ -114,7 +114,7 @@ func petasos(arguments []string) int {
 		decoratedHandler = alice.New(xcontext.Populate(requestFunc)).Then(redirectHandler)
 
 		_, petasosServer, done = webPA.Prepare(logger, nil, metricsRegistry, decoratedHandler)
-		signals                = make(chan os.Signal, 1)
+		signals                = make(chan os.Signal, 10)
 	)
 
 	_, err = monitor.New(
@@ -140,17 +140,12 @@ func petasos(arguments []string) int {
 		return 4
 	}
 
-	signal.Notify(signals)
+	signal.Notify(signals, os.Kill, os.Interrupt)
 	for exit := false; !exit; {
 		select {
 		case s := <-signals:
-			if s != os.Kill && s != os.Interrupt {
-				logger.Log(level.Key(), level.InfoValue(), logging.MessageKey(), "ignoring signal", "signal", s)
-			} else {
-				logger.Log(level.Key(), level.ErrorValue(), logging.MessageKey(), "exiting due to signal", "signal", s)
-				exit = true
-			}
-
+			logger.Log(level.Key(), level.ErrorValue(), logging.MessageKey(), "exiting due to signal", "signal", s)
+			exit = true
 		case <-done:
 			logger.Log(level.Key(), level.ErrorValue(), logging.MessageKey(), "one or more servers exited")
 			exit = true
